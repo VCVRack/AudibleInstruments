ebe533c6e9fb8d33362edfcb386ad406392d9c92
Andrew Belt <andrewpbelt@gmail.com>
2017-09-27 04:38:33 -0400

    Pointless refactoring

--- src/Clouds.cpp
+++ src/Clouds.cpp
@@ -77,11 +77,7 @@ void Clouds::step() {
 	if (!inputBuffer.full()) {
 		Frame<2> inputFrame;
 		inputFrame.samples[0] = getf(inputs[IN_L_INPUT]) * params[IN_GAIN_PARAM] / 5.0;
-		if(inputs[IN_R_INPUT])
-			inputFrame.samples[1] = getf(inputs[IN_R_INPUT]) * params[IN_GAIN_PARAM] / 5.0;
-		else
-			inputFrame.samples[1] = getf(inputs[IN_L_INPUT]) * params[IN_GAIN_PARAM] / 5.0;
-
+		inputFrame.samples[1] = inputs[IN_R_INPUT] ? getf(inputs[IN_R_INPUT]) * params[IN_GAIN_PARAM] / 5.0 : inputFrame.samples[0];
 		inputBuffer.push(inputFrame);
 	}
 
