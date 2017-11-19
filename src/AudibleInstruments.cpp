da3a76a390d18677751d76ed2be46131c2a43ba1
Andrew Belt <andrewpbelt@gmail.com>
2017-11-19 09:38:01 -0500

    Add tags

--- src/AudibleInstruments.cpp
+++ src/AudibleInstruments.cpp
@@ -10,19 +10,18 @@ void init(rack::Plugin *p) {
 	p->version = TOSTRING(VERSION);
 #endif
 
-	p->addModel(createModel<BraidsWidget>("Audible Instruments", "Braids", "Macro Oscillator"));
-	p->addModel(createModel<ElementsWidget>("Audible Instruments", "Elements", "Modal Synthesizer"));
-	p->addModel(createModel<TidesWidget>("Audible Instruments", "Tides", "Tidal Modulator"));
-	p->addModel(createModel<SheepWidget>("Audible Instruments", "Sheep", "Wavetable Oscillator"));
+	p->addModel(createModel<BraidsWidget>("Audible Instruments", "Braids", "Macro Oscillator", OSCILLATOR_TAG, WAVESHAPER_TAG));
+	p->addModel(createModel<ElementsWidget>("Audible Instruments", "Elements", "Modal Synthesizer", REVERB_TAG));
+	p->addModel(createModel<TidesWidget>("Audible Instruments", "Tides", "Tidal Modulator", LFO_TAG, OSCILLATOR_TAG, WAVESHAPER_TAG, FUNCTION_GENERATOR_TAG));
 	// p->addModel(createModel<StreamsWidget>("Audible Instruments", "Streams", "Dual Dynamics Gate"));
-	p->addModel(createModel<CloudsWidget>("Audible Instruments", "Clouds", "Texture Synthesizer"));
-	p->addModel(createModel<WarpsWidget>("Audible Instruments", "Warps", "Meta Modulator"));
+	p->addModel(createModel<CloudsWidget>("Audible Instruments", "Clouds", "Texture Synthesizer", GRANULAR_TAG, REVERB_TAG));
+	p->addModel(createModel<WarpsWidget>("Audible Instruments", "Warps", "Meta Modulator", RING_MODULATOR_TAG));
 	p->addModel(createModel<RingsWidget>("Audible Instruments", "Rings", "Resonator"));
-	p->addModel(createModel<LinksWidget>("Audible Instruments", "Links", "Multiples"));
-	p->addModel(createModel<KinksWidget>("Audible Instruments", "Kinks", "Utilities"));
-	p->addModel(createModel<ShadesWidget>("Audible Instruments", "Shades", "Mixer"));
-	p->addModel(createModel<BranchesWidget>("Audible Instruments", "Branches", "Bernoulli Gate"));
-	p->addModel(createModel<BlindsWidget>("Audible Instruments", "Blinds", "Quad VC-polarizer"));
-	p->addModel(createModel<VeilsWidget>("Audible Instruments", "Veils", "Quad VCA"));
-	p->addModel(createModel<FramesWidget>("Audible Instruments", "Frames", "Keyframer/Mixer"));
+	p->addModel(createModel<LinksWidget>("Audible Instruments", "Links", "Multiples", MULTIPLE_TAG, MIXER_TAG));
+	p->addModel(createModel<KinksWidget>("Audible Instruments", "Kinks", "Utilities", UTILITY_TAG));
+	p->addModel(createModel<ShadesWidget>("Audible Instruments", "Shades", "Mixer", MIXER_TAG));
+	p->addModel(createModel<BranchesWidget>("Audible Instruments", "Branches", "Bernoulli Gate", RANDOM_TAG, DUAL_TAG));
+	p->addModel(createModel<BlindsWidget>("Audible Instruments", "Blinds", "Quad VC-polarizer", MIXER_TAG, ATTENUATOR_TAG));
+	p->addModel(createModel<VeilsWidget>("Audible Instruments", "Veils", "Quad VCA", MIXER_TAG));
+	p->addModel(createModel<FramesWidget>("Audible Instruments", "Frames", "Keyframer/Mixer", MIXER_TAG, ATTENUATOR_TAG, LFO_TAG));
 }
