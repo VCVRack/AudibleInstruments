2fc0a0589b3d7b091d4cb7c3392a9deca202bb71
Andrew Belt <andrewpbelt@gmail.com>
2018-02-26 22:04:16 -0500

    Fix Warps panel

--- src/Warps.cpp
+++ src/Warps.cpp
@@ -131,7 +131,7 @@ struct WarpsWidget : ModuleWidget {
 		box.size = Vec(15*10, 380);
 		{
 			Panel *panel = new LightPanel();
-			panel->backgroundImage = Image::load(assetPlugin(plugin, "res/Blinds.png"));
+			panel->backgroundImage = Image::load(assetPlugin(plugin, "res/Warps.png"));
 			panel->box.size = box.size;
 			addChild(panel);
 		}
