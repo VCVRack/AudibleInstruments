7be622852a0b4b6a571d98a7a819743a45470e23
Andrew Belt <andrewpbelt@gmail.com>
2021-06-16 10:39:15 -0400

    Remove border from Rogan6PSLight.

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -32,5 +32,7 @@ template <typename Base>
 struct Rogan6PSLight : Base {
 	Rogan6PSLight() {
 		this->box.size = mm2px(Vec(23.04, 23.04));
+		// this->bgColor = nvgRGBA(0, 0, 0, 0);
+		this->borderColor = nvgRGBA(0, 0, 0, 0);
 	}
 };
