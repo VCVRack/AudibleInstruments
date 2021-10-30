7225bf8f79bcd937ace7b9e1fabf52dd9672b26e
Andrew Belt <andrewpbelt@gmail.com>
2021-10-30 03:22:39 -0400

    Fix unipolar level and CV knobs of Veils.

--- src/Veils.cpp
+++ src/Veils.cpp
@@ -42,8 +42,8 @@ struct Veils : Module {
 	Veils() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		for (int c = 0; c < 4; c++) {
-			configParam(GAIN1_PARAM + c, -1.0, 1.0, 0.0, string::f("Channel %d gain", c + 1), "%", 0, 100);
-			configParam(RESPONSE1_PARAM + c, -1.0, 1.0, 0.0, string::f("Channel %d response", c + 1));
+			configParam(GAIN1_PARAM + c, 0.0, 1.0, 0.0, string::f("Channel %d gain", c + 1), "%", 0, 100);
+			configParam(RESPONSE1_PARAM + c, 0.0, 1.0, 0.0, string::f("Channel %d response", c + 1));
 			configInput(IN1_INPUT + c, string::f("Channel %d", c + 1));
 			configInput(CV1_INPUT + c, string::f("Channel %d CV", c + 1));
 			configOutput(OUT1_OUTPUT + c, string::f("Channel %d", c + 1));
